b87bc168e69fdb3c7576e4402d99c11fb04dc6f0
Justin Chiu <justin_chiu@outlook.com>
2023-03-13 01:24:04 -0400

    Added underscore flashing code

--- js/global.js
+++ js/global.js
@@ -1,6 +1,25 @@
 console.log("global.js successfully loaded from desn2023.");
 
 
+
+
+
+// IMPORTANT VARIABLES
+
+let underscoreParams = { // underscore-related variables
+    class: ".underscore", // class of underscore span
+    endWidth: "7vw", // width of underscore span at the end of initial animation
+    delay: 7000, // when to start flashing
+    interval: 750 // length of flash
+}
+
+let showcaseParams = { // showcase-related variables
+    quantity: 9 // number of items to show
+}
+
+
+
+
 // UTILITY
 
 function shuffleArray(array) { // randomizes order of items in an array
@@ -12,81 +31,6 @@ function shuffleArray(array) { // randomizes order of items in an array
 
     return array;
 }
-
-
-// SHOWCASE ITEMS
-
-function showcase() { // shuffle, reduce, split and populate
-
-    // get elements
-
-    let showcaseItems = Array.from(document.querySelectorAll(".showcase__item:not(.is--placeholder)"));
-    let showcaseRows = document.querySelectorAll(".showcase__row"); // change class name if necessary
-
-    showcaseItems.forEach(function(item, i) {
-        item.id = "item-" + i;
-    });
-
-    if (showcaseItems.length > 0 && showcaseRows.length > 0) {
-
-        // randomize order
-
-        showcaseItems = shuffleArray(showcaseItems);
-
-
-        // reduce items
-
-        let quantity = 9; // number of items to show
-
-        while (showcaseItems.length > quantity) { // reduce array length to 9
-            showcaseItems.pop();
-        }
-
-
-        // split into groups
-
-        let rowGroups = new Array();
-
-        showcaseRows.forEach(function() { // rowGroups will have an array for each row
-            rowGroups.push(new Array());
-        });
-
-        showcaseItems.forEach(function(node, i) {
-
-            let group = Math.floor(i / showcaseRows.length);
-
-            // if showcaseRows.length == 3
-            // i:           0 1 2 3 4 5 6 7 8
-            // group:       0 0 0 1 1 1 2 2 2
-
-            rowGroups[group].push(node);
-
-        });
-
-        showcaseRows.forEach(function(row, i) {
-
-            rowGroups[i].forEach(function(item) {
-                row.appendChild(item);
-            });
-
-        });
-
-    }
-}
-
-
-// RUNNING
-
-let placeholders = document.querySelectorAll(".is--placeholder");
-placeholders.forEach(function(element) { // remove placeholders
-    element.remove();
-});
-
-window.onload = function() {
-    showcase(); // populate showcase
-}
-
-
 
 
 
@@ -142,4 +86,112 @@ function countdown() {
     clock('js-clock', deadline);
 }
 
-countdown();
+
+
+
+// SHOWCASE ITEMS
+
+function showcase() { // shuffle, reduce, split and populate
+
+    // get elements
+
+    let showcaseItems = Array.from(document.querySelectorAll(".showcase__item:not(.is--placeholder)"));
+    let showcaseRows = document.querySelectorAll(".showcase__row"); // change class name if necessary
+
+    showcaseItems.forEach(function(item, i) {
+        item.id = "item-" + i;
+    });
+
+    if (showcaseItems.length > 0 && showcaseRows.length > 0) {
+
+        // randomize order
+
+        showcaseItems = shuffleArray(showcaseItems);
+
+
+        // reduce items
+
+        let quantity = showcaseParams.quantity; // number of items to show
+
+        while (showcaseItems.length > quantity) { // reduce array length to 9
+            showcaseItems.pop();
+        }
+
+
+        // split into groups
+
+        let rowGroups = new Array();
+
+        showcaseRows.forEach(function() { // rowGroups will have an array for each row
+            rowGroups.push(new Array());
+        });
+
+        showcaseItems.forEach(function(node, i) {
+
+            let group = Math.floor(i / showcaseRows.length);
+
+            // if showcaseRows.length == 3
+            // i:           0 1 2 3 4 5 6 7 8
+            // group:       0 0 0 1 1 1 2 2 2
+
+            rowGroups[group].push(node);
+
+        });
+
+        showcaseRows.forEach(function(row, i) {
+
+            rowGroups[i].forEach(function(item) {
+                row.appendChild(item);
+            });
+
+        });
+
+    }
+}
+
+
+
+
+// UNDERSCORE FLASH
+
+function underscore() {
+
+    setTimeout(underscoreCheck, underscoreParams.delay);
+
+}
+
+function underscoreCheck(underscore = document.querySelector(underscoreParams.class)) {
+        
+    if (underscore.style.width == underscoreParams.endWidth) {
+            setInterval(underscoreFlash, underscoreParams.interval);
+            underscore.style.height = "";
+    } else {
+        setTimeout(underscoreCheck, 2000);
+    }
+
+}
+
+function underscoreFlash(underscore = document.querySelector(underscoreParams.class)) {
+
+    if (underscore.style.opacity == 1) {
+        underscore.style.opacity = 0;
+    } else {
+        underscore.style.opacity = 1;
+    }
+}
+
+
+
+
+// RUNNING
+
+let placeholders = document.querySelectorAll(".is--placeholder");
+placeholders.forEach(function(element) { // remove placeholders
+    element.remove();
+});
+
+window.onload = function() {
+    underscore();
+    countdown();
+    showcase();
+}
