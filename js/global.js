5d18b59754c6a831fc39b35a7ac4cb9befd51078
desn2023 <info@desn2023.com>
2023-03-14 23:46:08 -0400

    installed barba

---
+++ js/global.js
@@ -0,0 +1,15 @@
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave(data) {
+            return gsap.to(data.current.container, {
+                opacity: 0
+            });
+        },
+        enter(data) {
+            return gsap.from(data.next.container, {
+                opacity: 0
+            });
+        }
+    }]
+});
