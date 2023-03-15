fb74835096f393b81eaed822a2b4c3bcbcfd9d18
desn2023 <info@desn2023.com>
2023-03-15 01:16:26 -0400

    added live loader to barba

--- js/global.js
+++ js/global.js
@@ -1,7 +1,18 @@
+// Define the loading indicator element
+const loader = document.querySelector('#loader');
+
 barba.init({
     preventRunning: true,
     transitions: [{
         name: 'opacity-transition',
+        beforeEnter() {
+            // Show the loading indicator
+            loader.style.display = 'block';
+        },
+        afterEnter() {
+            // Hide the loading indicator
+            loader.style.display = 'none';
+        },
         leave(data) {
             return gsap.to(data.current.container, {
                 opacity: 0
@@ -15,8 +26,12 @@ barba.init({
             return gsap.from(data.next.container, {
                 opacity: 0
             });
+        },
+        onProgress: (state) => {
+            // Update the loading indicator based on the progress of the transition
+            loader.style.width = state.progress * 100 + '%';
         }
     }]
 });
 
-console.log("this is live");
+console.log("loader update");
