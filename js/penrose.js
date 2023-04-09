260605ffde10f955e8b26833d0f841eaaf4a3b6b
Justin Chiu <justin_chiu@outlook.com>
2023-04-08 20:24:01 -0400

    Semi-working svg interactions

--- js/penrose.js
+++ js/penrose.js
@@ -1,31 +1,85 @@
 // do not touch, Justin is working on this
 
-let penrose = {
+const penrose = {
+    zoneRadius: 100, // distance at which the transition is triggered
+
     wrapper: document.querySelector("#penrose-wrapper"),
     svg: document.querySelector("#penrose-svg"),
+    zonesGroup: document.querySelector("#penrose-zones-group"),
+    zones: new Array(),
     sects: document.querySelectorAll(".sect"),
     origW: 1066,
     origH: 1045
 }
 
+penrose.zoneEnter = function (e) {
+    sectID = parseInt(e.currentTarget.getAttribute("sectid"));
+    console.log("!");
+
+    gsap.to(penrose.sects[sectID], {
+        fillOpacity: 1,
+        duration: 0.25
+    });
+}
+
+penrose.zoneLeave = function (e) {
+
+}
+
+penrose.sectEnter = function (e) {
+
+}
+
+penrose.sectLeave = function (e) {
+
+}
+
 penrose.setSize = function () {
 
-    let wrapH = penrose.wrapper.clientHeight;
-    let svgH = penrose.svg.getAttribute("height");
+    const wrapH = penrose.wrapper.clientHeight;
+    const svgH = penrose.svg.getAttribute("height");
 
-    let scale = wrapH / svgH;
+    const scale = wrapH / svgH;
 
     penrose.wrapper.style.width = (wrapH * penrose.origW / penrose.origH) + "px";
     penrose.svg.style.transform = "scale(" + scale + ")";
 
 }
 
+penrose.initSect = function (sect, index) {
+
+    // get centre coords of sector
+    const bbox = sect.getBBox();
+    const centreX = bbox.x + bbox.width / 2;
+    const centreY = bbox.y + bbox.height / 2;
+
+    // create zone shape
+    const zoneShape = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    zoneShape.classList.add("sect-zone");
+    zoneShape.setAttribute("sectid", index);
+
+    // set centre of circle to same coords as centre of sector
+    zoneShape.setAttribute("cx", centreX);
+    zoneShape.setAttribute("cy", centreY);
+
+    // circle radius is distance at which the transition is triggered
+    zoneShape.setAttribute("r", penrose.zoneRadius);
+
+    // events
+    sect.onmouseover = penrose.sectEnter;
+    sect.onmouseout = penrose.sectLeave;
+    zoneShape.onmouseover = penrose.zoneEnter;
+    zoneShape.onmouseout = penrose.zoneLeave;
+
+    // add to svg
+    penrose.zonesGroup.appendChild(zoneShape);
+    // add to list
+    penrose.zones.push(zoneShape);
+
+}
+
 penrose.init = function () {
-
-    // sizing
-    penrose.setSize();
-
-    // create zone shapes
-    
+    penrose.setSize(); // sizing
+    penrose.sects.forEach(penrose.initSect); // create zone shapes set event handlers
 }
 
