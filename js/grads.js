7fd6cebbe47564fef635d0441682bb9849739cf9
Justin Chiu <justin_chiu@outlook.com>
2023-04-18 19:18:06 -0400

    css fixes

--- js/grads.js
+++ js/grads.js
@@ -235,7 +235,12 @@ dyncontent.toggleAllAnim = function (instant = false) {
         n = 1000;
     }
 
-    let tl = gsap.timeline();
+    let tl = gsap.timeline({
+        onComplete: function () {
+            const toggleFilter = wrapper.querySelector(".toggle.is--filter");
+            toggleFilter.style.opacity = "";
+        }
+    });
 
     tl.to(".filter__wrapper__wrapper", {
         duration: Math.min(0.2, n),
@@ -300,7 +305,12 @@ dyncontent.toggleAllClick = function (e) {
 
 dyncontent.toggleFilterClick = function (e) {
 
-    let tl = gsap.timeline();
+    let tl = gsap.timeline({
+        onComplete: function () {
+            const toggleAll = wrapper.querySelector(".toggle.is--all");
+            toggleAll.style.opacity = "";
+        }
+    });
 
     tl.to(e.currentTarget, {
         duration: 0.2,
